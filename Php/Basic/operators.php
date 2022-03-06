1e6f126582829f21121b1f68ee34900bcef39458
Chandan Gupta Bhagat <chandan.bhagat@outlook.com>
2022-03-07 04:59:13 +0545

    operators explained

---
+++ Php/Basic/operators.php
@@ -0,0 +1,26 @@
+<?php 
+//Arithmetic operators
+    //Unary operator ++, -- // works on one operand
+    //Binary operator +, -, *, /, %, // works on 2 operands
+    //ternary operator ?: Works on 3 operands
+
+    //Assignment operators
+    //=, +=, -+ *= /= %= // works on 2 operands example : $c+=2;
+
+    //string operator
+    //.   // works on 2 operands
+    $a="Kshitiz";
+    $b="Wagle";
+    $fullname=$a." ".$b; // in js + is concatenation operator
+    //.=  // works on 2 operands
+    $fullname="Khsitiz";
+    $fullname.=" Wagle";
+
+    //logical operator
+    //&&, ||, ! // works on 2 operands
+    //and or not    // works on 2 operands
+    //xor  // works on 2 operands
+
+    //comparison operator
+    //==, !=, ===, !==, <, >, <=, >= // works on 2 operands
+?>
