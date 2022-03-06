606efdc3e43cf15b8a4f3ca543c25d7cd1f5924c
Chandan Gupta Bhagat <chandan.bhagat@outlook.com>
2022-03-07 04:01:57 +0545

    numbers explained

---
+++ Php/Basic/numbers.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Numbers</title>
+</head>
+<body>
+    <?php 
+        //Integeer
+        $a=10;
+        $b=20;
+        $oct=010;
+        $hex=0x10;
+        $bin=0b1010;
+        echo $bin;
+        echo "<br>";
+
+        //floating point
+        $c=10.5;
+        $d=2.1e5;
+        echo $d;
+    ?>
+</body>
+</html>
