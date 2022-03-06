4e015273028dd3d8700f4a554d290d2c017d0096
Chandan Gupta Bhagat <chandan.bhagat@outlook.com>
2022-03-07 04:59:23 +0545

    looping explained

---
+++ Php/Basic/looping.php
@@ -0,0 +1,19 @@
+<?php 
+    for ($i=0; $i < 10; $i++) { 
+        # code...
+    }
+    while ($a <= 10) {
+        # code...
+    }
+    do{
+
+    }while($a<=10);
+
+
+    $day=array("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday");
+    foreach ($day as $d) {
+        # code...
+        echo $d;
+        echo "<br>";
+    }
+?>
